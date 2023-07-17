644bb4df885c92bc0ddba7a7fc0fbb6463ea82b5
Arthur Conmy <conmy.a@northeastern.edu>
2023-07-17 21:03:33 +0000

    Arthur upd

--- transformer_lens/rs/arthurs_notebooks/arthur_dla.py
+++ transformer_lens/rs/arthurs_notebooks/arthur_dla.py
@@ -16,12 +16,13 @@ else:
 
 # %%
 
-model = HookedTransformer.from_pretrained("gpt2")
+model = HookedTransformer.from_pretrained("gpt2", device="cpu")
 NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX = NEG_HEADS[model.cfg.model_name]
+# NEGATIVE_LAYER_IDX, NEGATIVE_HEAD_IDX = 9, 9
 
 #%%
 
-mybatch, mytargets = get_filtered_webtext()
+mybatch, mytargets = get_filtered_webtext(model=model, device="cpu")
 
 #%%
 
@@ -30,26 +31,40 @@ head_activations = model.run_with_cache(
     mybatch,
     names_filter = lambda name: name == get_act_name("result", NEGATIVE_LAYER_IDX)
 )[1][get_act_name("result", NEGATIVE_LAYER_IDX)][:, :, NEGATIVE_HEAD_IDX]
-
 mean_activations = head_activations.mean(dim=(0, 1))
+print(mean_activations.norm())
 
 #%%
 
-for subtract_mean in [False, True]:
-    current_activations = head_activations - (mean_activations if subtract_mean else 0.0)
+K = 5
+
+for subtract_mean in [True, False]:
+    current_activations = head_activations.clone() - (mean_activations.clone() if subtract_mean else 0.0)
+    print(current_activations.norm())
+    current_activations_layer_normed = (current_activations / current_activations.norm(dim=-1, keepdim=True)) * np.sqrt(model.cfg.d_model)
+
+    print(current_activations_layer_normed.norm(), model.W_U.norm())
 
     current_attributions = einops.einsum(
-        current_activations,
+        current_activations_layer_normed,
         model.W_U,
         "batch seq_len d_model, d_model d_vocab -> batch seq_len d_vocab",
     )
 
-    K = 5
-    top_attributions = torch.topk(
+    top_attribution_indices = torch.topk(
         current_attributions.abs(),
         k=K,
         dim=-1,
     ).indices
-    assert list(top_attributions.shape ) == [1, K]
+
+    assert list(top_attribution_indices.shape) == [mybatch.shape[0], mybatch.shape[1], K]
+
+    top_attribution_values = current_attributions[ # i hate gather
+        torch.arange(mybatch.shape[0]).unsqueeze(-1).unsqueeze(-1),
+        torch.arange(mybatch.shape[1]).unsqueeze(-1).unsqueeze(0),
+        top_attribution_indices,
+    ]
+
+    print("Proportion of positive logit attributions:", ((torch.nn.functional.relu(top_attribution_values) > 0).int().sum() / top_attribution_values.numel()).item())
 
 # %%
