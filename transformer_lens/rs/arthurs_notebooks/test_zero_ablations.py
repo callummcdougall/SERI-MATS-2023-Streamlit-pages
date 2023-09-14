1ab7f55f0ec2b546ae1adc7aea4511352b4a02d6
Arthur Conmy <conmy.a@northeastern.edu>
2023-09-14 11:42:30 +0000

    Commit -progress verifying GPT-Neo weirdness

--- transformer_lens/rs/arthurs_notebooks/test_zero_ablations.py
+++ transformer_lens/rs/arthurs_notebooks/test_zero_ablations.py
@@ -1,5 +1,7 @@
 #%%
 
+# Mega quick that the GPT-Neo-125M result is not fake! (It's not)
+
 from transformer_lens.cautils.notebook import *
 
 # %%
@@ -17,13 +19,19 @@ my_batch, my_targets = get_filtered_webtext(model, batch_size=30, seed= 1729, de
 
 # %%
 
-def compute_loss(model):
-    predictions = model(
+def compute_loss(model, return_att=False):
+    predictions, cache = model.run_with_cache(
         my_batch[:10],
+        names_filter = lambda name: name == "blocks.0.attn.hook_pattern" and return_att,
     )
     logprobs = predictions.log_softmax(dim=-1)
     neglogprobs = -logprobs[torch.arange(len(predictions)).unsqueeze(1), torch.arange(predictions.shape[1]).unsqueeze(0), my_targets[:10]]
-    return neglogprobs.mean()
+
+    if return_att:
+        return neglogprobs.mean(), list(cache.values())[0]
+
+    else:
+        return neglogprobs.mean()
 
 # %%
 
@@ -38,6 +46,7 @@ for head in [2, 3, 5]:
         f"blocks.0.attn.hook_result",
         zer,
     )
-    loss = compute_loss(model)
+    loss, att = compute_loss(model, return_att=True)
     print(loss)
+
 # %%
