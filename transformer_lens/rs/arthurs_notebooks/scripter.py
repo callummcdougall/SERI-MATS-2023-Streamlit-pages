76b688400845b7c35f88716d194d942036048556
Arthur Conmy <conmy.a@northeastern.edu>
2023-09-20 14:25:16 +0000

    Script micro change

--- transformer_lens/rs/arthurs_notebooks/scripter.py
+++ transformer_lens/rs/arthurs_notebooks/scripter.py
@@ -12,14 +12,14 @@ def run_script(threshold, gpu_id):
 
 if __name__ == '__main__':
 
-    num_gpus = 6  # specify the number of GPUs available
+    num_gpus = 4  # specify the number of GPUs available
     num_jobs_per_gpu = 1  # specify the number of jobs per GPU
 
     pool = multiprocessing.Pool(num_gpus * num_jobs_per_gpu)
     jobs = []
 
     for it in range(3, int(1e6)):
-        curspace = list(range(2000, 500, -20))
+        curspace = list(range(0, 2200, 20))
 
         if not isinstance(curspace, list):
             curspace = curspace[1:-1]
