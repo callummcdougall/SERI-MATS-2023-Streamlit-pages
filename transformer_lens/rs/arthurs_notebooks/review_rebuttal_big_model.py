16c992e88e9c24ada30fb95ce825887b190ed639
Arthur Conmy <conmy.a@northeastern.edu>
2023-11-14 11:14:56 +0000

    Prog

--- transformer_lens/rs/arthurs_notebooks/review_rebuttal_big_model.py
+++ transformer_lens/rs/arthurs_notebooks/review_rebuttal_big_model.py
@@ -1,33 +1,50 @@
 #%%
 
+"""
+WARNING: This should use TRANSFORMERLENS 1.9.0 not this repo...
+"""
+
 import os
 os.environ["TRANSFORMERS_CACHE"] = "/workspace/cache/"
 import transformers
-from transformer_lens.cautils.notebook import *
-from transformer_lens.rs.arthurs_notebooks.arthurs_utils import *
-import transformers
-import datasets
+import torch
+
+# from transformer_lens.cautils.notebook import *
+# from transformer_lens.rs.arthurs_notebooks.arthurs_utils import *
+# import datasets
+
 import gc
 
-#%% # Note 3 minute delay
-
-model_name = "mistralai/Mistral-7B-v0.1"
-
-#%%
-
-model = transformer_lens.HookedTransformer.from_pretrained_no_processing(model_name)
+# #%% # Note 3 minute delay
+# model_name = "mistralai/Mistral-7B-v0.1"
 
 #%%
 
-if False:
-    model = transformers.AutoModelForCausalLM.from_pretrained(model_name) # Hopefully works
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+model_name = "gpt2" # "huggyllama/llama-7b"
+torch.set_grad_enabled(False)
 
 #%%
 
-print(model) # What do the modules look like? We'll try to fold the LN bias.
+from transformer_lens import HookedTransformer
+import torch
 
-#%% # Takes 90 secs
+#%%
+
+# from_pretrained version 4 minutes
+if "llama" in model_name.lower() or "mistral" in model_name.lower():
+    hf_model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
+    hf_tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    model = HookedTransformer.from_pretrained_no_processing("llama-7b", hf_model=hf_model, device="cpu", fold_ln=False, center_writing_weights=False, center_unembed=False, tokenizer=hf_tokenizer)
+
+else:
+    model = HookedTransformer.from_pretrained_no_processing(model_name, device="cpu", fold_ln=False, center_writing_weights=False, center_unembed=False)
+
+#%%
+
+print(model)
+# What do the modules look like? We'll try to fold the LN bias
+
+#%% # Takes 90 secs. And in TL >150 seconds
 
 model = model.to(torch.bfloat16) # Load into lower precision...
 gc.collect()
@@ -41,9 +58,10 @@ torch.cuda.empty_cache()
 
 # %%
 
-# Sanity check that we can run forward passes 
+if False:
+    # Sanity check that we can run forward passes 
+    tokenizer = model.tokenizer if TL else transformers.AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
 
-tokenizer = transformers.AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
 my_sentence = """Successorship behavior: the successor head pushes for the successor of a token in the context. We
 say this behavior occurs when one of the top-5-attended tokens is in the successorship dataset, and
 the correct next token is the successor of t.
@@ -57,160 +75,137 @@ of the top-5-attended tokens.
 Greater-than behavior: the successor head pushes for a token greater than a previous token in the
 context. We say this behavior occurs when we do not observe successorship behavior, but when the
 correct next token is still part of an ordinal sequence and has greater order than some top-5-attended
-token (e.g. if the successor head attends to the token ‘first’ and the model predicts the token ‘third’.)"""
-my_tokens = tokenizer.encode(my_sentence, return_tensors="pt").cuda() # First loss is really high but whatever, it's really low on average
+token (e.g. if the successor head attends to the token ‘first’ and the model predicts the token
+‘third’.)"""
+
+if False:
+    my_tokens = tokenizer.encode(my_sentence, return_tensors="pt").cuda() # First loss is really high but whatever, it's really low on average
+else:
+    my_tokens = model.to_tokens(my_sentence).cuda()
 
 #%%
 
-my_logits = model(my_tokens).logits
+if False:
+    my_logits = hf_model(my_tokens).logits
+else:
+    my_logits = model(my_tokens)
 my_logprobs = my_logits.log_softmax(dim=-1)
 my_neglogprobs = -my_logprobs
 my_loss = my_neglogprobs[0, torch.arange(my_neglogprobs.shape[1]-1), my_tokens[0][1:]]
-print(my_loss.mean()) # Yeah pretty small
+print(my_loss.mean()) # Yeah pretty small... 2 for LLAMA-7B...
 
 #%%
 
 # Probably use the `streaming` option on this...
 hf_dataset_name = "suolyer/pile_pile-cc"
 # Dataset
-
-#%%
-
-hf_iter_dataset = iter(datasets.load_dataset(hf_dataset_name, streaming=True))
-
-#%%
+import datasets
+hf_iter_dataset = iter(datasets.load_dataset(hf_dataset_name, streaming=True)["validation"])
 
 # %%
 
-# From https://github.com/UlisseMini/activation_additions_hf/blob/cb418413ac5319d0ec698b84744257e7306a4f18/activation_additions/__init__.py#L15
-
-from contextlib import contextmanager
-from typing import Tuple, List, Callable, Optional
-import torch as t
-import torch.nn as nn
-
-
-# types
-PreHookFn = Callable[[nn.Module, t.Tensor], Optional[t.Tensor]]
-Hook = Tuple[nn.Module, PreHookFn]
-Hooks = List[Hook]
-
-
-@contextmanager
-def pre_hooks(hooks: Hooks):
-    """
-    Context manager to register pre-forward hooks on a list of modules. The hooks are removed when the context exits.
-    """
-    try:
-        handles = [mod.register_forward_pre_hook(hook) for mod, hook in hooks]
-        yield handles
-    finally:
-        for handle in handles:
-            handle.remove()
-
-
-def get_blocks(model: nn.Module) -> nn.ModuleList:
-    """
-    Get the ModuleList containing the transformer blocks from a model.
-    """
-    def numel_(mod):
-        return sum(p.numel() for p in mod.parameters())
-    model_numel = numel_(model)
-    canidates = [
-        mod
-        for mod in model.modules()
-        if isinstance(mod, nn.ModuleList) and numel_(mod) > .5*model_numel
-    ]
-    assert len(canidates) == 1, f'Found {len(canidates)} ModuleLists with >50% of model params.'
-    return canidates[0]
-
-
-@contextmanager
-def residual_stream(model: nn.Module, layers: Optional[List[int]] = None) -> List[t.Tensor]:
-    """
-    Context manager to store residual stream activations in the model at the specified layers.
-    Alternatively "model(..., output_hidden_states=True)" can be used, this is more flexible though and works with model.generate().
-    """
-
-    stream = [None] * len(get_blocks(model))
-    layers = layers or range(len(stream))
-    def _make_hook(i):
-        def _hook(_, inputs):
-            # concat along the sequence dimension
-            stream[i] = inputs[0] if stream[i] is None else t.cat([stream[i], inputs[0]], dim=1)
-        return _hook
-
-    hooks = [(layer, _make_hook(i)) for i, layer in enumerate(get_blocks(model)) if i in layers]
-    with pre_hooks(hooks):
-        yield stream
-
-
-def _device(model):
-    "Get the device of the first parameter of the model. Assumes all parameters are on the same device."
-    return next(model.parameters()).device
-
-
-def get_vectors(model: nn.Module, tokenizer, prompts: List[str], layer: int, padding=True):
-    """
-    Get the activations of the prompts at the specified layer. Used later for activation additions.
-    """
-    with residual_stream(model, layers=[layer]) as stream:
-        inputs = tokenizer(prompts, return_tensors='pt', padding=padding)
-        device = _device(model)
-        inputs = {k: v.to(device) for k, v in inputs.items()}
-        _ = model(**inputs)
-
-    return stream[layer]
-
-
-def get_diff_vector(model: nn.Module, tokenizer, prompt_add: str, prompt_sub: str, layer: int):
-    """
-    Get the difference vector between the activations of prompt_add and prompt_sub at the specified layer. 
-    Convinience function for
-        s = get_vectors(..., prompts)
-        return s[0] - s[1]
-    """
-    stream = get_vectors(model, tokenizer, [prompt_add, prompt_sub], layer)
-    return (stream[0] - stream[1]).unsqueeze(0)
-
-
-def get_hook_fn(act_add: t.Tensor) -> PreHookFn:
-    """
-    Get a hook function that adds an activation addition vector.
-    """
-
-    def _hook(_: nn.Module, inputs: Tuple[t.Tensor]):
-        resid_pre, = inputs
-        if resid_pre.shape[1] == 1:
-            return None # caching for new tokens in generate()
-
-        # We only add to the prompt (first call), not the generated tokens.
-        ppos, apos = resid_pre.shape[1], act_add.shape[1]
-        assert apos <= ppos, f"More mod tokens ({apos}) then prompt tokens ({ppos})!"
-
-        # TODO: Make this a function-wrapper for flexibility.
-        resid_pre[:, :apos, :] += act_add
-        return resid_pre
-
-    return _hook
+# Step 1 --- Run up to Layer L (end of Succ Head)
+# Step 2 --- Calculate the Mean Ablated Head Output, etc
+# Step 3 continue forward pass
 
 #%%
 
-# get_blocks(model)
+def get_filtered_dataset(model, batch_size=30, seed: int = 1729, device="cuda", max_seq_len=1024):
+    hf_dataset_name = "suolyer/pile_pile-cc" # Probably somewhere in LLAMA data
+    hf_iter_dataset = iter(datasets.load_dataset(hf_dataset_name, streaming=True)["validation"])
 
-get_vectors_output = get_vectors(
-    model,
-    tokenizer,
-    ["The quick brown fox jumped over the lazy dog."],
-    layer=0,
-    padding=False, # Single token crap for now
-)
+    filtered_tokens = []
+    targets = []  # targets for prediction
+
+    print("Not rapid, but not THAT slow :-) ")
+    _idx = -1
+    while len(filtered_tokens) < batch_size:
+        _idx += 1
+        cur_tokens = model.to_tokens(next(hf_iter_dataset)["text"], truncate=False).tolist()[0]
+        if (
+            len(cur_tokens) > max_seq_len # Greater Than so that we have all the targets for the context!!!
+        ):  # so we're not biasing towards early sequence positions...
+            filtered_tokens.append(cur_tokens[:max_seq_len])
+            targets.append(cur_tokens[1 : max_seq_len + 1])
+
+    mybatch = torch.LongTensor(filtered_tokens).to(device)
+    mytargets = torch.LongTensor(targets).to(device)
+    return mybatch, mytargets
+
+tokens, targets = get_filtered_dataset(model, batch_size=2, max_seq_len=1024)
 
 # %%
 
-with residual_stream(model, [0]) as f:
-    pass
+import itertools
+for layer, head in list(itertools.product([model.cfg.n_layers-1, model.cfg.n_layers-2], list(range(model.cfg.n_heads)))):
+    print("HEAD!!!", layer, head)
 
-print(f)
+    import warnings
+    warnings.warn("Not short tokens")
+
+    output, cache = model.run_with_cache(
+        tokens,
+        names_filter=[
+            # f"blocks.{layer}.attn.hook_rot_q",
+            # f"blocks.{layer}.attn.hook_rot_k",
+            f"blocks.{layer}.attn.hook_pattern",          # Reconstruct head output from these
+            f"blocks.{layer}.attn.hook_v",       # Logit lens god I hope works
+        ],
+        stop_at_layer = layer + 1,
+    )
+
+    # Implement just "suppression" here, no filtering.
+
+    z_for_head = cache[f"blocks.{layer}.attn.hook_v"][:, :, head]
+    assert len(z_for_head.shape)==3 # batch, seq_len, dhead
+    pattern = cache[f"blocks.{layer}.attn.hook_pattern"][:, head]
+    import einops
+    ov_per_position = einops.einsum(
+        z_for_head,
+        model.W_O[layer, head],
+        "batch seq_len dhead, d_head d_model -> batch seq_len d_model",
+    )
+
+
+    # Delete the dumb shit 
+
+    dumb_shit = einops.einsum(
+        ov_per_position,
+        pattern.to(ov_per_position.dtype), # DOwncastr 
+        "batch key_index d_model, batch query_index key_index -> batch query_index d_model",
+    )
+
+
+    # clever_shit = project(
+    #     ov_per_position, # Shape batch key_index d_model
+    #     model.W_U.T[short_tokens], # batch key_index d_model
+    # )
+
+    # Well that sucks
+
+    unnormed_directions = model.W_U.T[tokens]
+    normed_directions = unnormed_directions / unnormed_directions.norm(dim=-1, keepdim=True)
+    projection_sizes = einops.einsum(
+        ov_per_position,    
+        normed_directions,
+        "batch key_index d_model, batch key_index d_model -> batch key_index d_model"
+    )
+    projection = projection_sizes * normed_directions
+    norms = ov_per_position.norm(dim=-1)
+
+    # Okay just replace at end of model???
+    # Or just see the sizes of these damn thing
+    # Do like: where attention is >0.5 and not BOS, check how big the projections are
+
+    positions = (pattern>0.5) & (torch.arange(pattern.shape[-1])[None, None] > 0).to(pattern.device)
+
+
+    for b, q, k in positions.nonzero():
+        cur_projection = projection_sizes[b, q, k]
+        norm = norms[b, k]
+        print(cur_projection / norm)
+
+    gc.collect()
+    torch.cuda.empty_cache() # Is this actually big???
 
 # %%
